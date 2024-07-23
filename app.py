53d18277a0fd77cec20bff5132312425f8b746f4
AdriannaDJ <atroussard88@gmail.com>
2024-07-22 20:09:42 -0700

    added sql reading in data code.

--- app.py
+++ app.py
@@ -1,22 +1,45 @@
 from flask import Flask, render_template, request
-import pickle
 import pandas as pd
+import sqlite3
+import pickle
 import json
 import os
 import re
 
-# create app
 app = Flask(__name__)
 
-# load assets
+# Load ML model and preprocessing objects
 with open('training/model.pkl', 'rb') as f: 
-    model = pickle.load(f)    
+    model = pickle.load(f)
 with open('training/scaler.pkl', 'rb') as f: 
     scaler = pickle.load(f)
 with open('training/choices.pkl', 'rb') as f: 
     choices = pickle.load(f)
 with open('training/ohe.pkl', 'rb') as f: 
     ohe = pickle.load(f)
+
+# SQLite database connection
+conn = sqlite3.connect('employee_data.db')
+cursor = conn.cursor()
+
+# Function to create SQLite table and insert data
+def create_and_insert():
+    # Read CSV into pandas DataFrame
+    df = pd.read_csv('Resources/watson_healthcare_modified.csv')
+
+    # Remove unnecessary columns
+    df = df.drop(columns=['EmployeeID', 'EmployeeCount', 'StandardHours', 'TrainingTimesLastYear', 'MonthlyRate', 'DailyRate', 'HourlyRate', 'Over18'])
+    
+    # Rename columns
+    df.rename(columns={'Age':'Age (18-60)', 'DistanceFromHome':'Distance From Home (miles)', 'Education':'Education (1-5)', 'EnvironmentSatisfaction':'Env. Satisfaction (1-4)',
+                   'JobInvolvement':'Job Involvement (1-4)', 'JobLevel':'Job Level (1-5)', 'JobSatisfaction':'Job Satisfaction (1-4)', 'PerformanceRating':'Performance Rating (1-4)',
+                   'RelationshipSatisfaction':'Relationship Satisfaction (1-4)', 'Shift':'Shift (1-3)', 'WorkLifeBalance':'WorkLifeBalance (1-4)'}, inplace=True)
+    
+    # Create SQL table
+    df.to_sql('employee_data', conn, if_exists='replace', index=False)
+
+# Call the function to create table and insert data
+create_and_insert()
     
 # get list of categorical and numerical columns
 cat_cols = [col for col in choices.keys() if choices[col] != None]
